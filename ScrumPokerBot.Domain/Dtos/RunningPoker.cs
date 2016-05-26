97957f77cafddac85e7ac044128acc283ed86c9a
Benjamin Hahn <hahn.benjamin@gmx.de>
2016-05-26 09:37:10 +0200

    RunningPoker

--- ScrumPokerBot.Domain/Dtos/RunningPoker.cs
+++ ScrumPokerBot.Domain/Dtos/RunningPoker.cs
@@ -7,23 +7,18 @@ namespace ScrumPokerBot.Domain.Dtos
 {
     public class RunningPoker
     {
-        private readonly ScrumPokerSession session;
-
         public RunningPoker(ScrumPokerSession session)
         {
-            this.session = session;
             Users = session.AllUsers.Select(u => new UserEstimation(u)).ToList();
-       
-        }
+         }
 
         
         public List<UserEstimation> Users { get;}
 
         public override string ToString()
         {
-            var poker = session.Poker;
-            var x = poker.Users.GroupBy(u => u.Estimation).OrderBy(e => e.Key);
-            var results = x.ToDictionary(k => k.Key, v => v.Count());
+            var groupedEstimations = this.Users.GroupBy(u => u.Estimation).OrderBy(e => e.Key);
+            var results = groupedEstimations.ToDictionary(k => k.Key, v => v.Count());
             var sb = new StringBuilder();
             foreach (var result in results)
             {
@@ -32,19 +27,26 @@ namespace ScrumPokerBot.Domain.Dtos
 
             if (results.Count > 1)
             {
-                var lowestEstimation = results.First().Key;
-                var highestEstimation= results.Last().Key;
-
-                var highest = poker.Users.Where(u => u.Estimation == highestEstimation).OrderBy(e => Guid.NewGuid()).First();
-                var lowest = poker.Users.Where(u => u.Estimation == lowestEstimation).OrderBy(e => Guid.NewGuid()).First();
-
-                sb.AppendLine();
-                sb.AppendLine($"Niedrigste Schätzung {lowest.Estimation} von {lowest.User}");
-                sb.AppendLine($"Höchste Schätzung {highest.Estimation} von {highest.User}");
+                sb.AppendLine(AppendHighestAndLowest(results));
             }
 
             return sb.ToString();
         }
 
+        private string AppendHighestAndLowest(Dictionary<int, int> results)
+        {
+            var sb = new StringBuilder();
+            var lowestEstimation = results.First().Key;
+            var highestEstimation = results.Last().Key;
+
+            var highest = this.Users.Where(u => u.Estimation == highestEstimation).OrderBy(e => Guid.NewGuid()).First();
+            var lowest = this.Users.Where(u => u.Estimation == lowestEstimation).OrderBy(e => Guid.NewGuid()).First();
+
+            sb.AppendLine();
+            sb.AppendLine($"Niedrigste Schätzung {lowest.Estimation} von {lowest.User}");
+            sb.AppendLine($"Höchste Schätzung {highest.Estimation} von {highest.User}");
+
+            return sb.ToString();
+        }
     }
 }
