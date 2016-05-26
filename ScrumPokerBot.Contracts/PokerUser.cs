6ce500d43eabc254b69064478312702343ffd49c
Benjamin Hahn <hahn.benjamin@gmx.de>
2016-05-26 09:34:55 +0200

    PokerUser ToString

--- ScrumPokerBot.Contracts/PokerUser.cs
+++ ScrumPokerBot.Contracts/PokerUser.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Telegram.Bot.Types;
 
 namespace ScrumPokerBot.Contracts
@@ -20,12 +21,41 @@ namespace ScrumPokerBot.Contracts
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public long ChatId { get; set; }
+        public int Id => (int) ChatId;
 
         public override string ToString()
         {
-            return $"{Lastname}, {Firstname} ({Username})";
-        }
+            var sb = new StringBuilder();
+            if (!string.IsNullOrEmpty(Lastname))
+            {
+                sb.Append(Lastname);
+                if (!string.IsNullOrEmpty(Firstname))
+                {
+                    sb.Append($", {Firstname}");
+                }
+                if (!string.IsNullOrEmpty(Username))
+                {
+                    sb.Append($" (@{Username})");
+                }
+                return sb.ToString();
+            }
 
-        public int Id => (int) ChatId;
+            if (!string.IsNullOrEmpty(Firstname))
+            {
+                sb.Append(Firstname);
+
+                if (!string.IsNullOrEmpty(Username))
+                {
+                    sb.Append($" (@{Username})");
+                }
+                return sb.ToString();
+            }
+
+            if (!string.IsNullOrEmpty(Username))
+            {
+                sb.Append($"@{Username}");
+            }
+            return sb.ToString();
+        }
     }
 }
